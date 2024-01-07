ffd589d2536e18857546cc1ceaf5378473a43b8a
Afnan Mir <mirafnan12@gmail.com>
2024-01-07 10:30:13 -0600

    Add 2023 experience

--- src/components/Home/Header.js
+++ src/components/Home/Header.js
@@ -14,7 +14,7 @@ export default function Header() {
             </div>
             <div className="text-center">
                     <p>
-                        Hello my name is Afnan Mir, and I am a third year student at the University of Texas, studying Electrical and Computer Engineering. 
+                        Hello my name is Afnan Mir, and I am a fourth year student at the University of Texas, studying Electrical and Computer Engineering. 
                         My career interests surround the topics of software engineering, machine learning, and natural language processing, while my hobbies center around
                         basketball, working out, and various outdoor activities
                     </p>
